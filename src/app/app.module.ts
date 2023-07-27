f7918f0ac7dccee6b41a79987dec727050d1d726
Vaagn Avanesyan <vahagn1993@gmail.com>
2023-07-27 14:48:27 +0300

    chore(*): add Akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './components/app';
+import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AkitaNgDevtools.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
